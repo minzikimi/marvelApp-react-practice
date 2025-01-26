2cc89a22513a5c9d71e7802aa1756128c801cea1
Minji Kim <minzikimi@Minjis-MacBook-Pro.local>
2025-01-26 23:45:53 +0100

    first commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,53 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Movies from "../components/Movies"; // Movies 컴포넌트를 import 해야 합니다
+
+function Detail() {
+  const { id } = useParams(); // id를 추출합니다
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+  const apiURL = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  const getMovie = async() => {
+    try {
+      const response = await fetch(apiURL);
+      const json = await response.json();
+      setMovie(json.data.movie);
+      setLoading(false);
+      console.log(json.data.movie);
+    } catch (error) {
+      console.error("Error fetching movie:", error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="container">
+    {loading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div>
+        <h1>Detail</h1>
+        {movie ? (
+          <Movies
+            key={movie.id}
+            id={movie.id}
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}
+          />
+        ) : (
+          <h1>Movie not found</h1>
+        )}
+      </div>
+    )}
+  </div>
+  );
+}
+
+export default Detail;
